b207cab30afea563d56783edfb759a5a4e600d5d
6clc <569519574@qq.com>
2019-11-17 19:23:29 +0800

    add dataloader manager

--- cframe/learner/basic_learner.py
+++ cframe/learner/basic_learner.py
@@ -1,13 +1,17 @@
 from cframe.models import ModelManager
 import os
 import tensorflow  as tf
+from tensorflow.keras.callbacks import ModelCheckpoint
 
 class BasicLearner(object):
     def __init__(self, model_manager: ModelManager,
                  dl_manager,
-                 loss, optimizer, metrics):
+                 loss, optimizer, metrics, callbacks=None):
+        if callbacks is None:
+            callbacks = []
         self.model_manager = model_manager
         self.dl_manager = dl_manager
+        self.callbacks = callbacks
 
         self.model = self.model_manager.get_model()
         self.train_dl = self.dl_manager.get_train_dl()
@@ -15,16 +19,18 @@ class BasicLearner(object):
         self.test_dl = self.dl_manager.get_test_dl()
 
         self.summary_writer_dir = None
-        self.writer = None
 
-        self.running_score = dict()
-        self.best_para = None
+        self._init()
+
+        checkpoint = ModelCheckpoint(filepath=os.path.join(self.summary_writer_dir, 'best.hdf5'),
+                                     monitor='val_acc', save_best_only=True, verbose=1, mode='max')
+        self.callbacks.append(checkpoint)
 
         self.model.compile(optimizer=optimizer,
                            loss=loss,
                            metrics=metrics)
 
-        self._init()
+
 
     def _init(self):
         root_dir = '/'.join(self.dl_manager.config['root_dir'].split('/')[:-1]) + '/SummaryWriter'
