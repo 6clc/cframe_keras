de5cffcde021321faafc51d26f806387849572b2
6clc <569519574@qq.com>
2019-11-18 15:20:22 +0800

    add dataloader manager

--- colab.py
+++ colab.py
@@ -1,24 +1,32 @@
-from cframe.dataloader import DataConfiger
-from cframe.dataloader import ClassificationDataloaderManager
-from cframe.learner import BasicLearner
+from tensorflow.keras.applications.resnet50 import preprocess_input
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+
+
 from cframe.models import ModelConfiger
 from cframe.models import ModelManager
 import os
 
 root_dir = '/content/drive/My Drive'
-
-data_config = DataConfiger.get_data_config('garbage')
-data_config['root_dir'] = os.path.join(root_dir, 'Data')
-data_config['batch_size'] = 32
-dl_manager = ClassificationDataloaderManager(data_config)
+BATCH_SIZE_TRAINING = 100
+num_epochs = 5
+num_images = 14802
+STEPS_PER_EPOCH_TRAINING = num_images // BATCH_SIZE_TRAINING
 
 model_config = ModelConfiger.get_model_config('resnet50')
 model_manager = ModelManager(model_config)
+model = model_manager.get_model()
 
-learner = BasicLearner(model_manager, dl_manager,
-                       loss='sparse_categorical_crossentropy',
-                       optimizer='adam',
-                       metrics=['accuracy'])
+image_size = 224
+data_generator = ImageDataGenerator(preprocessing_function=preprocess_input)
+train_generator = data_generator.flow_from_directory(
+        os.path.join(root_dir, 'Data/DataSets/garbage_classify/imgs'),
+        target_size=(image_size, image_size),
+        batch_size=BATCH_SIZE_TRAINING,
+        class_mode='categorical')
 
+fit_history = model.fit_generator(
+        train_generator,
+        steps_per_epoch=STEPS_PER_EPOCH_TRAINING,
+        epochs = num_epochs,
+)
 
-learner.train(5)
